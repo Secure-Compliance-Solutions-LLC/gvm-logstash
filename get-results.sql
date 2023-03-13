65da19e268978c2d3f6e72dfa6596d96e64cee37
Joshua Schmitt <joshua@pixelcubed.com>
2023-03-13 11:27:40 -0500

    Update get-results.sql

--- get-results.sql
+++ get-results.sql
@@ -1,5 +1,5 @@
 select r.host                                                                               as "ip",
-       r.hostname                                                                           as "hostname",
+       rhd_hostname.value                                                                   as "hostname",
        rhd_mac.value                                                                        as "mac",
        rhd_os_txt.value                                                                     as "os_txt",
        rhd_os_cpe.value                                                                     as "os_cpe",
@@ -10,27 +10,27 @@ select r.host                                                                   
        split_part(r.port, '/', 2)                                                           as "port_protocol",
        r.severity                                                                           as "cvss",
        (case
-            when r.severity <= 0 then 'Log'
-            when r.severity >= 0.1 and r.severity <= 3.9
+            when r.severity <= 0::real then 'Log'
+            when r.severity >= 0.1::real and r.severity <= 3.9::real
                 then 'Low'
-            when severity >= 4.0 and severity <= 6.9
+            when r.severity >= 4.0::real and r.severity <= 6.9::real
                 then 'Medium'
-            when severity >= 7.0 and severity <= 10.0
+            when r.severity >= 7.0::real and r.severity <= 10.0::real
                 then 'High' end)                                                            as "severity",
        (case
-            when r.severity <= 0
+            when r.severity <= 0::real
                 then 1
             else 0 end)                                                                     as "log",
        (case
-            when r.severity >= 0.1 and r.severity <= 3.9
+            when r.severity >= 0.1::real and r.severity <= 3.9::real
                 then 1
             else 0 end)                                                                     as "low",
        (case
-            when severity >= 4.0 and severity <= 6.9
+            when severity >= 4.0::real and severity <= 6.9::real
                 then 1
             else 0 end)                                                                     as "medium",
        (case
-            when severity >= 7.0 and severity <= 10.0
+            when severity >= 7.0::real and severity <= 10.0::real
                 then 1
             else 0 end)                                                                     as "high",
        r.qod                                                                                as "qod",
@@ -93,8 +93,6 @@ select r.host                                                                   
                 then 'VMware Workstation'
             when n.name like 'VMware Player %'
                 then 'VMware Player'
-            when n.name like 'VMware Tools %'
-                then 'VMware Tools'
             when n.name like 'Huawei EulerOS %'
                 then 'Huawei EulerOS'
             when n.name like 'Cisco ASA %' or n.name like 'Cisco Adaptive Security Appliance %'
@@ -202,11 +200,9 @@ select r.host                                                                   
                 then 'Apple iCloud'
             when n.name like 'Apple Mac OS X %' or n.name like 'Apple MacOSX %'
                 then 'Apple Mac OS X'
-            when n.name like 'PuTTY %'
-                then 'PuTTY'
             when n.name like 'nginx %' or n.name like 'Nginx %'
                 then 'nginx'
-            when n.name like 'Jetty %' or n.name like 'Eclipse Jetty %'
+            when n.name like 'Jetty %'
                 then 'Jetty'
             when n.name like 'Docker %'
                 then 'Docker'
@@ -226,10 +222,6 @@ select r.host                                                                   
                 then 'OpenVPN'
             when n.name like 'OpenSSH %'
                 then 'OpenSSH'
-            when n.name like 'Dropbear %'
-                then 'Dropbear'
-            when n.name like 'Lighttpd %'
-                then 'Lighttpd'
             when n.name like 'Perl %'
                 then 'Perl'
             when n.name like 'Ruby on Rails %'
@@ -284,8 +276,6 @@ select r.host                                                                   
                 then 'Asterisk'
             when n.name like 'Java %' or n.name like 'Oracle Java %'
                 then 'Java'
-            when n.name like 'jQuery %'
-                then 'jQuery'
             when n.name like 'OpenSSL%'
                 then 'OpenSSL'
             when n.name like 'Webmin %'
@@ -296,8 +286,6 @@ select r.host                                                                   
                 then 'Magento'
             when n.name like 'F5 BIG-IP %'
                 then 'F5 BIG-IP'
-            when n.name like 'MiniUPnP %'
-                then 'MiniUPnP'
             when n.name like 'Splunk %'
                 then 'Splunk'
             when n.name like 'Serv-U %'
@@ -311,24 +299,18 @@ select r.host                                                                   
             when n.name like 'IBM Db2 %'
                 then 'IBM Db2'
             when n.name like 'Microsoft Internet Explorer %' or n.name like 'Microsoft IE %' or n.name like 'IE %' or
-                 n.name like 'Microsoft Explorer %' or n.name like 'MS IE %'
+                 n.name like 'Microsoft Explorer %'
                 then 'Microsoft Internet Explorer'
             when n.name like 'Microsoft .NET Framework %'
                 then 'Microsoft .NET Framework'
             when n.name like '.NET Core %'
                 then '.NET Core'
-            when n.name like 'ASP.NET %'
-                then 'ASP.NET'
             when n.name like 'Microsoft SQL Server %'
                 then 'Microsoft SQL Server'
             when n.name like 'Microsoft SharePoint %' or n.name like 'MS SharePoint Server %'
                 then 'Microsoft SharePoint Server'
             when n.name like 'Microsoft Exchange %'
                 then 'Microsoft Exchange Server'
-            when n.name like 'Microsoft IIS %' or n.name like 'IIS %'
-                then 'Microsoft IIS'
-            when n.name like 'Microsoft Visual Studio %'
-                then 'Microsoft Visual Studio'
             when n.name like 'Microsoft Lync %'
                 then 'Microsoft Lync'
             when n.name like 'Microsoft Visio %'
@@ -365,7 +347,7 @@ select r.host                                                                   
                 then 'Apache OpenOffice'
             when n.name like 'Apache Hadoop %'
                 then 'Apache Hadoop'
-            when n.name like 'Apache HTTP%' or n.name like 'Apache Web Server %'
+            when n.name like 'Apache HTTP%'
                 then 'Apache HTTP Server'
             when n.name like 'Apache Struts %'
                 then 'Apache Struts'
@@ -375,8 +357,6 @@ select r.host                                                                   
                 then 'ProFTPD'
             when n.name like 'vsftpd %'
                 then 'vsftpd'
-            when n.name like 'Zoom Client %'
-                then 'Zoom Client'
             when n.name like 'Foxit Reader %' or n.name like '% Foxit Reader %' or n.affected like 'Foxit Reader%'
                 then 'Foxit Reader'
             when n.name like 'MongoDB %'
@@ -388,21 +368,40 @@ from results r
          left join report_hosts rh on r.host = rh.host and r.report = rh.report
          left join (select *
                     from report_host_details
-                    where id in (select max(id) from report_host_details group by report_host, name)) rhd_os_txt
-                   on rhd_os_txt.name = 'best_os_txt' and rh.id = rhd_os_txt.report_host
+                    where id in (select max(id)
+                                 from report_host_details
+                                 where name = 'hostname'
+                                 group by report_host, name)) rhd_hostname
+                   on rh.id = rhd_hostname.report_host
          left join (select *
                     from report_host_details
-                    where id in (select max(id) from report_host_details group by report_host, name)) rhd_os_cpe
-                   on rhd_os_cpe.name = 'best_os_cpe' and rh.id = rhd_os_cpe.report_host
+                    where id in (select max(id)
+                                 from report_host_details
+                                 where name = 'best_os_txt'
+                                 group by report_host, name)) rhd_os_txt
+                   on rh.id = rhd_os_txt.report_host
          left join (select *
                     from report_host_details
-                    where id in (select max(id) from report_host_details group by report_host, name)) rhd_smb_auth
-                   on (rhd_smb_auth.name = 'Auth-SMB-Failure' or rhd_smb_auth.name = 'Auth-SMB-Success') and
-                      rh.id = rhd_smb_auth.report_host
+                    where id in (select max(id)
+                                 from report_host_details
+                                 where name = 'best_os_cpe'
+                                 group by report_host, name)) rhd_os_cpe
+                   on rh.id = rhd_os_cpe.report_host
          left join (select *
                     from report_host_details
-                    where id in (select max(id) from report_host_details group by report_host, name)) rhd_mac
-                   on rhd_mac.name = 'MAC' and rh.id = rhd_mac.report_host
+                    where id in (select max(id)
+                                 from report_host_details
+                                 where name = 'Auth-SMB-Failure'
+                                    or name = 'Auth-SMB-Success'
+                                 group by report_host, name)) rhd_smb_auth
+                   on rh.id = rhd_smb_auth.report_host
+         left join (select *
+                    from report_host_details
+                    where id in (select max(id)
+                                 from report_host_details
+                                 where name = 'MAC'
+                                 group by report_host, name)) rhd_mac
+                   on rh.id = rhd_mac.report_host
          left join (select array_agg(value) as cpes, report_host
                     from report_host_details
                     where name = 'App'
@@ -419,6 +418,6 @@ from results r
                    on n.oid = cve.vt_oid
          left join (select array_agg(ref_id) as ids, vt_oid from vt_refs where type = 'url' group by vt_oid) url
                    on n.oid = url.vt_oid
-where r.id > :sql_last_value
-order by r.id
-limit 10000
+where re.scan_run_status = 1
+  and r.qod > 70
+  and to_timestamp(r.date) > now() - interval '2 months'
